4557b0a12060c8c1caeee2a9176da4a21614e448
rgilyard <gilyardrachel@gmail.com>
2022-03-11 17:11:08 -0800

    Add loops-data-service

--- src/app/loops/loops.page.ts
+++ src/app/loops/loops.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Loop } from '../models/loop.model';
 import {HourMinSec} from "../models/hour-min-sec.model";
+import {LoopsDataService} from "../services/loops-data.service";
 
 
 @Component({
@@ -11,115 +12,14 @@ import {HourMinSec} from "../models/hour-min-sec.model";
 export class LoopsPage implements OnInit {
   loopsData: Loop[] = new Array();
 
-  constructor() {
-    // Add fake data to test display. Two loops with two timers each.
-    this.loopsData = [{
-      loopName: "Homework",
-      timerList: [{
-        timerName: "Math",
-        initialLength: {
-          hours: 0,
-          minutes: 1,
-          seconds: 30
-        },
-        adjustment: {
-          hours: 0,
-          minutes: 0,
-          seconds: 15
-        },
-        finalLength: {
-          hours: 0,
-          minutes: 1,
-          seconds: 0
-        },
-        alwaysInclude: true,
-        playInterval: 0,
-        vibrate: true,
-        alarmSound: "sound",
-        alarmVolume: 15,
-        autoStart: false
-      },
-      {
-        timerName: "Break",
-        initialLength: {
-          hours: 0,
-          minutes: 1,
-          seconds: 30
-        },
-        adjustment: {
-          hours: 0,
-          minutes: 0,
-          seconds: 15
-        },
-        finalLength: {
-          hours: 0,
-          minutes: 1,
-          seconds: 0
-        },
-        alwaysInclude: true,
-        playInterval: 0,
-        vibrate: true,
-        alarmSound: "sound",
-        alarmVolume: 15,
-        autoStart: false
-      }],
-      currentTimerIndex: 0
-    },
-    {
-      loopName: "Chores",
-      timerList: [{
-        timerName: "Clean",
-        initialLength: {
-          hours: 0,
-          minutes: 1,
-          seconds: 30
-        },
-        adjustment: {
-          hours: 0,
-          minutes: 0,
-          seconds: 15
-        },
-        finalLength: {
-          hours: 0,
-          minutes: 1,
-          seconds: 0
-        },
-        alwaysInclude: true,
-        playInterval: 0,
-        vibrate: true,
-        alarmSound: "sound",
-        alarmVolume: 15,
-        autoStart: false
-      },
-      {
-        timerName: "Break",
-        initialLength: {
-          hours: 0,
-          minutes: 1,
-          seconds: 30
-        },
-        adjustment: {
-          hours: 0,
-          minutes: 0,
-          seconds: 15
-        },
-        finalLength: {
-          hours: 0,
-          minutes: 1,
-          seconds: 0
-        },
-        alwaysInclude: true,
-        playInterval: 0,
-        vibrate: true,
-        alarmSound: "sound",
-        alarmVolume: 15,
-        autoStart: false
-      }],
-      currentTimerIndex: 0
-    }]
+  constructor(private loopsDataService: LoopsDataService) {
   }
 
   ngOnInit() {
+    // Get the fake user loops data
+    this.loopsData = this.loopsDataService.getLoopsData();
   }
+
+
 
 }
--- src/app/services/loops-data.service.ts
+++ src/app/services/loops-data.service.ts
@@ -1,10 +1,126 @@
 import { Injectable } from '@angular/core';
+import {Loop} from "../models/loop.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoopsDataService {
+  loopsData: Loop[] = new Array();
 
+  constructor() {
+    // Add fake data to test display. Two loops with two timers each.
+    this.loopsData = [{
+      loopName: "Homework",
+      timerList: [{
+        timerName: "Math",
+        initialLength: {
+          hours: 0,
+          minutes: 1,
+          seconds: 30
+        },
+        adjustment: {
+          hours: 0,
+          minutes: 0,
+          seconds: 15
+        },
+        finalLength: {
+          hours: 0,
+          minutes: 1,
+          seconds: 0
+        },
+        alwaysInclude: true,
+        playInterval: 0,
+        vibrate: true,
+        alarmSound: "sound",
+        alarmVolume: 15,
+        autoStart: false
+      },
+      {
+        timerName: "Break",
+        initialLength: {
+          hours: 0,
+          minutes: 1,
+          seconds: 30
+        },
+        adjustment: {
+          hours: 0,
+          minutes: 0,
+          seconds: 15
+        },
+        finalLength: {
+          hours: 0,
+          minutes: 1,
+          seconds: 0
+        },
+        alwaysInclude: true,
+        playInterval: 0,
+        vibrate: true,
+        alarmSound: "sound",
+        alarmVolume: 15,
+        autoStart: false
+      }],
+      currentTimerIndex: 0
+    },
+    {
+      loopName: "Chores",
+      timerList: [{
+        timerName: "Clean",
+        initialLength: {
+          hours: 0,
+          minutes: 1,
+          seconds: 30
+        },
+        adjustment: {
+          hours: 0,
+          minutes: 0,
+          seconds: 15
+        },
+        finalLength: {
+          hours: 0,
+          minutes: 1,
+          seconds: 0
+        },
+        alwaysInclude: true,
+        playInterval: 0,
+        vibrate: true,
+        alarmSound: "sound",
+        alarmVolume: 15,
+        autoStart: false
+      },
+      {
+        timerName: "Break",
+        initialLength: {
+          hours: 0,
+          minutes: 1,
+          seconds: 30
+        },
+        adjustment: {
+          hours: 0,
+          minutes: 0,
+          seconds: 15
+        },
+        finalLength: {
+          hours: 0,
+          minutes: 1,
+          seconds: 0
+        },
+        alwaysInclude: true,
+        playInterval: 0,
+        vibrate: true,
+        alarmSound: "sound",
+        alarmVolume: 15,
+        autoStart: false
+      }],
+      currentTimerIndex: 0
+    }]
+  }
 
-  constructor() { }
+  /*
+  * The getLoopsData function returns a user's loops and timers settings.
+  *
+  * This is used in the Loops Page.
+  * */
+  getLoopsData() {
+    return this.loopsData;
+  }
 }
